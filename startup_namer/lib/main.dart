fd84444c1bb0cb203acfa30a2858a09a4b029236
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2022-11-17 22:49:53 -0300

    adicionada dependência de palavras em inglês e colocadas na tela de forma aleatória

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -1,18 +1,27 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random(); // Add this line.
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello world'),
+        body: Center(                          // Drop the const, and
+          //child: Text('Hello World'),        // Replace this text...
+          child: Text(wordPair.asPascalCase,
+              style: TextStyle(fontSize: 50)),
+          // With this text.
         ),
       ),
     );
